7310b0b27830d0d77cd64552a81e033aac23a63e
faizin-blockcode <85869040+thunder-chain@users.noreply.github.com>
2022-08-05 22:46:54 +1000

    diamond implementation

--- contracts/libraries/LibAppStorage.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-struct AppStorage {
-    address _mediaContract; 
-    // Mapping from owner to operator approvals
-    mapping(address => mapping(address => bool)) _operatorApprovals;
-}
