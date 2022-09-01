2c2b2e81469baa1c248e3f7f9adb0556c9c7f572
BlockyMan <mohammadh.chinaei@gmail.com>
2022-09-02 02:42:19 +0430

    Final changes

--- diamon-2-hardhat/hardhat.config.js
+++ diamon-2-hardhat/hardhat.config.js
@@ -7,6 +7,7 @@ require('@nomiclabs/hardhat-waffle')
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
 
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
 	solidity: "0.8.9",
